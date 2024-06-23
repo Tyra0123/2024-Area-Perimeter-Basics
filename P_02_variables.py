80f0b5cad79b0a851497595608f1ce2f39990db2
Tyra0123 <xuet3230@masseyhigh.school.nz>
2024-06-24 11:56:36 +1200

    Variable Basics

---
+++ P_02_variables.py
@@ -0,0 +1,20 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 1
+num_string = "5"
+
+# Variables can be Boolean (True / False)
+love_programming = True
+
+print(greeting)
+print(fav_num)
+print(love_programming)
+print(fav_num * 2)
+print(num_string * 5)
+
+# This variable has a problem!
+my_name = "Tyra"
